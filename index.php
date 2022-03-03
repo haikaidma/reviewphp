6be446c4e1885a43720c7dc4f62907b5debf9e67
haikaidma <haivan141#gmail.com>
2022-03-03 09:45:36 +0700

    change

--- index.php
+++ index.php
@@ -14,30 +14,30 @@
 <h2>Ngày 1</h2>
 
 <a href="ngay1/bai1.php">bài 1<a>
-<a href='/reviewphp/ngay1/bai2.php'>bài 2<a>
-<a href='/reviewphp/ngay1/bai3.php'>bài 3<a>
-<a href='/reviewphp/ngay1/bai4.php'>bài 4<a>
-<a href='/reviewphp/ngay1/bai5.php'>bài 5<a>    
-<a href='/reviewphp/ngay1/bai6.php'>bài 6<a>
-<a href='/reviewphp/ngay1/bai7.php'>bài 7<a>
-<a href='/reviewphp/ngay1/bai8.php'>bài 8<a>
-<a href='/reviewphp/ngay1/bai9.php'>bài 9<a>
-<a href='/reviewphp/ngay1/bai10.php'>bài 10<a>
-<a href='/reviewphp/ngay1/bai11.php'>bài 11<a>
+<a href="ngay1/bai2.php">bài 2<a>
+<a href="ngay1/bai3.php">bài 3<a>
+<a href="ngay1/bai4.php">bài 4<a>
+<a href="ngay1/bai5.php">bài 5<a>    
+<a href="ngay1/bai6.php">bài 6<a>
+<a href="ngay1/bai7.php">bài 7<a>
+<a href="ngay1/bai8.php">bài 8<a>
+<a href="ngay1/bai9.php">bài 9<a>
+<a href="ngay1/bai10.php">bài 10<a>
+<a href="ngay1/bai11.php">bài 11<a>
 <h2>Ngày 2</h2>
-<a href='/reviewphp/ngay2/bai1.php'>bài 1<a>
-<a href='/reviewphp/ngay2/bai2.php'>bài 2<a>
-<a href='/reviewphp/ngay2/bai3.php'>bài 3<a>
-<a href='/reviewphp/ngay2/bai4.php'>bài 4<a>
-<a href='/reviewphp/ngay2/bai5.php'>bài 5<a>
-<a href='/reviewphp/ngay2/bai6.php'>bài 6<a>
-<a href='/reviewphp/ngay2/bai7.php'>bài 7<a>
-<a href='/reviewphp/ngay2/bai8.php'>bài 8<a>
-<a href='/reviewphp/ngay2/bai9.php'>bài 9<a>
-<a href='/revwphiep/ngay2/bai10.php'>bài 10<a>
-<a href='/revwphiep/ngay2/bai11.php'>bài 11<a>
+<a href="ngay2/bai1.php">bài 1<a>
+<a href="ngay2/bai2.php">bài 2<a>
+<a href="ngay2/bai3.php">bài 3<a>
+<a href="ngay2/bai4.php">bài 4<a>
+<a href="ngay2/bai5.php">bài 5<a>
+<a href="ngay2/bai6.php">bài 6<a>
+<a href="ngay2/bai7.php">bài 7<a>
+<a href="ngay2/bai8.php">bài 8<a>
+<a href="ngay2/bai9.php">bài 9<a>
+<a href="ngay2/bai10.php">bài 10<a>
+<a href="ngay2/bai11.php">bài 11<a>
 <h2>Ngày 3</h2>
-<a href='/reviewphp/ngay3/dangnhap.php'>Form login<a>
+<a href="ngay3/dangnhap.php">Form login<a>
 <?php
 
 ?>
